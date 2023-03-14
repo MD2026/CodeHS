c5d66509cfb3655536155a409aa79365393b426b
MD2026 <126593996+MD2026@users.noreply.github.com>
2023-03-14 12:24:07 -0400

    Create 246.py

---
+++ Turtle Graphics/246.py
@@ -0,0 +1,14 @@
+penup()
+backward(200)
+right(90)
+forward(200)
+left(90)
+
+for i in range (3):
+    forward(100)
+    left(90)
+    pendown()
+    forward(400)
+    penup()
+    backward(400)
+    right(90)
